d740a5132d8bfbc58ff91f3301fc89e9bb043558
Vlasov Artem [Lenovo] <root@proscript.ru>
2016-09-24 16:52:14 +0400

    Функция split

--- vincode/__init__.py
+++ vincode/__init__.py
@@ -4,3 +4,4 @@ from __future__ import unicode_literals, absolute_import
 from .validation import validate, find_incorrect_symbols, IncorrectVinException
 from .parser import parse, ParseException
 from .raw import translate
+from .vincode import *
